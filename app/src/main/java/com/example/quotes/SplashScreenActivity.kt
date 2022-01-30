25bb721aea9884c8dba79e0b607af548598e5ba7
eduVieiraAF <edu_vieira@hotmail.com>
2022-01-30 19:49:28 -0300

    Animation
    Just a little animation.

--- app/src/main/java/com/example/quotes/SplashScreenActivity.kt
+++ app/src/main/java/com/example/quotes/SplashScreenActivity.kt
@@ -1,10 +1,12 @@
 package com.example.quotes
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 
+@SuppressLint("CustomSplashScreen")
 class SplashScreenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
